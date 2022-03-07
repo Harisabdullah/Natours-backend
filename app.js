0b0b43979abcb8984e0246e2393c0b605da5aeff
Harisabdullah <hariskhan1435@gmail.com>
2022-03-07 19:37:49 +0500

    Initial commit

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
 // Middlewares
-console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV ==='development'){
   app.use(morgan('dev'));
 }
@@ -15,10 +15,7 @@ if (process.env.NODE_ENV ==='development'){
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`))
 
-app.use((req, res, next) => {
-  console.log('Hello from the middleware');
-  next();
-})
+
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -32,6 +29,20 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+app.all('*', (req,res,next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`), 404);
+})
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+  })
+  next();
+})
 
 module.exports = app;
 
