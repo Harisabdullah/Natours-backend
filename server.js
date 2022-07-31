c2fcf729dbf0610701b0019440b17dd7ea03d84c
harisabdullah <hariskhan1435@gmail.com>
2022-07-31 16:58:30 +0500

    Preparing for Deployment

--- server.js
+++ server.js
@@ -25,7 +25,8 @@ mongoose
 
 1
 // Start server
-const port = process.env.PORT || 8000;
+const port = process.env.PORT;
+// const port = process.env.PORT || 8000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
